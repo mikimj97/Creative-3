024c0ad259c5d143abbeb9d60b14e0d4e5ef8277
mikimj97 <mikimj97@gmail.com>
2018-10-18 15:47:26 +0000

    Lists added but remove not working yet

--- Search.js
+++ Search.js
@@ -30,79 +30,66 @@ angular.module('Search', ['ui.router'])
         'postFactory',
         function($scope, $http, postFactory) {
 
-            var ingredients = [];
-
-            $scope.results = postFactory.results;
+            $scope.ingredients = [];
+            $scope.favorites = [];
+            $scope.p2 = true;
+            $scope.div1 = true;
+            $scope.results = postFactory.results; //Do we need this?
 
             $scope.addToList = function() {
-                console.log("List");
-                ingredients.push($scope.formContent);
-                console.log($scope.formContent);
+                if ($scope.formContent === '') { return; }
+                if ($scope.div1) $scope.div1 = false;
+                $scope.ingredients.push($scope.formContent);
+                
+                //console.log($scope.formContent);
+                console.log($scope.ingredients);
                 $scope.formContent = '';
             };
 
             $scope.searchFood = function() {
-                console.log("HI!");
-                var searches = ingredients.join();
-                console.log(searches);
-                // if ($scope.formContent === '') { return; }
-                var link = 'https://www.food2fork.com/api/search?key=22ba8c696ee83f4a051ad6b1c350dcff&q=' + searches
-                console.log(link);
+                var searches = $scope.ingredients.join();
+                //console.log(searches);
+                var link = 'https://www.food2fork.com/api/search?key=22ba8c696ee83f4a051ad6b1c350dcff&q=' + searches;
+                //console.log(link);
                 $http.get(link).
                 then(function(response) {
-                    console.log(response.data);
+                    //console.log(response.data);
 
                     for (var i = 0; i < response.data.count; i++) {
-                        console.log(response.data[i]);
-                        console.log('Title: ' + response.data.recipes[i].title);
+                        //console.log(response.data[i]);
+                        //console.log('Title: ' + response.data.recipes[i].title);
                         $scope.results.push({
                             title: response.data.recipes[i].title,
                             url: response.data.recipes[i].source_url,
                             f2f: response.data.recipes[i].f2f_url,
                             rank: response.data.recipes[i].social_rank
-                            //   upvotes: 0,
-                            //   comments: [
-                            //   ]
                         });
                     }
-                    ingredients = [];
-
+                    
+                    
+                    
+                    $scope.ingredients = [];
+                    $scope.p1 = true;
+                    $scope.div1 = true;
                     $scope.formContent = '';
-                    // $scope.greeting = response.data;
                 });
             };
-
-            //   $scope.incrementUpvotes = function(post) {
-            //     post.upvotes += 1;
-            //   };
-
+            
+            $scope.addToFavorites = function(recipe) {
+                if ($scope.p2) { $scope.p2 = false; }
+                $scope.favorites.push(recipe);
+            };
+            
+            $scope.removeIngredient = function(recipe) {
+                console.log(recipe);
+                console.log($scope.ingredients.indexOf(recipe));
+                $scope.ingredients.splice(0, $scope.ingredients.indexOf(recipe));
+            };
+            
+            $scope.removeFavorite = function(recipe) {
+                console.log(recipe);
+                console.log($scope.favorites.indexOf(recipe));
+                $scope.favorites.splice(0, $scope.favorites.indexOf(recipe));
+            }
         }
     ])
-
-// .controller('Hello', function($scope, $http) {
-//     $http.get('https://www.food2fork.com/api/search?key=22ba8c696ee83f4a051ad6b1c350dcff&q=chicken,cheese,rice').
-//     then(function(response) {
-//         console.log(response.data);
-//         $scope.greeting = response.data;
-//     });
-// });
-// .controller('PostsCtrl', [
-// '$scope',
-// '$stateParams',
-// 'postFactory',
-// function($scope, $stateParams, postFactory){
-//   $scope.post = postFactory.posts[$stateParams.id];
-
-//   $scope.addComment = function(){
-//     if($scope.body === '') { return; }
-//     $scope.post.comments.push({
-//       body: $scope.body,
-//       upvotes: 0
-//     });
-//     $scope.body = '';
-//   };
-
-//   $scope.incrementUpvotes = function(comment){
-//     comment.upvotes += 1;
-//   };
-// }]);
