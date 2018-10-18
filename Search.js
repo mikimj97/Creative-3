5c586ea9bfe84e34c7c3f2d6b825b7bd197f9c73
Douglas Uriona <duriona@gmail.com>
2018-10-18 22:48:52 +0000

    Fixed a bug where the search result wouldn't update and added pictures

--- Search.js
+++ Search.js
@@ -9,11 +9,6 @@ angular.module('Search', ['ui.router'])
                     templateUrl: '/home.html',
                     controller: 'MainCtrl'
                 });
-            // .state('posts', {
-            //   url: '/posts/{id}',
-            //   templateUrl: '/posts.html',
-            //   controller: 'PostsCtrl'
-            // });
 
             $urlRouterProvider.otherwise('home');
         }
@@ -34,39 +29,34 @@ angular.module('Search', ['ui.router'])
             $scope.favorites = [];
             $scope.p2 = true;
             $scope.div1 = true;
-            $scope.results = postFactory.results; //Do we need this?
+            $scope.results = postFactory.results; //Do we need this? (I think so -Douglas)
 
             $scope.addToList = function() {
                 if ($scope.formContent === '') { return; }
                 if ($scope.div1) $scope.div1 = false;
                 $scope.ingredients.push($scope.formContent);
-                
-                //console.log($scope.formContent);
-                console.log($scope.ingredients);
+
                 $scope.formContent = '';
             };
 
             $scope.searchFood = function() {
+                console.log('In serach food');
+                console.log('array' + $scope.ingredients);
                 var searches = $scope.ingredients.join();
-                //console.log(searches);
-                var link = 'https://www.food2fork.com/api/search?key=22ba8c696ee83f4a051ad6b1c350dcff&q=' + searches;
-                //console.log(link);
+                var link = 'https://www.food2fork.com/api/search?key=d02ad68f83bf27815c12516a7f7262cc&q=' + searches;
                 $http.get(link).
                 then(function(response) {
-                    //console.log(response.data);
+                    $scope.results = [];
 
                     for (var i = 0; i < response.data.count; i++) {
-                        //console.log(response.data[i]);
-                        //console.log('Title: ' + response.data.recipes[i].title);
                         $scope.results.push({
                             title: response.data.recipes[i].title,
                             url: response.data.recipes[i].source_url,
                             f2f: response.data.recipes[i].f2f_url,
-                            rank: response.data.recipes[i].social_rank
+                            rank: response.data.recipes[i].social_rank,
+                            image: response.data.recipes[i].image_url
                         });
                     }
-                    
-                    
                     
                     $scope.ingredients = [];
                     $scope.p1 = true;
@@ -74,18 +64,18 @@ angular.module('Search', ['ui.router'])
                     $scope.formContent = '';
                 });
             };
-            
+
             $scope.addToFavorites = function(recipe) {
                 if ($scope.p2) { $scope.p2 = false; }
                 $scope.favorites.push(recipe);
             };
-            
+
             $scope.removeIngredient = function(recipe) {
                 console.log(recipe);
                 console.log($scope.ingredients.indexOf(recipe));
                 $scope.ingredients.splice($scope.ingredients.indexOf(recipe), 1);
             };
-            
+
             $scope.removeFavorite = function(recipe) {
                 console.log(recipe);
                 console.log($scope.favorites.indexOf(recipe));
